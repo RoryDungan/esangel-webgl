518e06014cf074b53da486c689a942e41fd0bff9
Rory Dungan <rorydungan@gmail.com>
2018-08-15 18:29:53 +1000

    Example 2: render triangles instead of random points

---
+++ Chapter1/Gasket2DTriangles/gasket2.js
@@ -0,0 +1,71 @@
+'use strict'
+
+/**
+ * Sierpinski gasket function. Generates triangles for the specified number of
+ * iterations.
+ * @param {number} iterations
+ */
+const sierpinski = function (iterations) {
+  const divideTriangle = function* (a, b, c, count) {
+    if (count <= 0) {
+      yield [a, b, c]
+      return
+    }
+
+    const newCount = count - 1,
+      ab = mix(a, b, 0.5),
+      ac = mix(a, c, 0.5),
+      bc = mix(b, c, 0.5)
+
+    yield *divideTriangle(a, ab, ac, newCount)
+    yield *divideTriangle(c, ac, bc, newCount)
+    yield *divideTriangle(b, bc, ab, newCount)
+  }
+
+  const vertices = [
+    vec2(-1, -1),
+    vec2( 0,  1),
+    vec2( 1, -1)
+  ]
+
+  return [...divideTriangle(...vertices, iterations)]
+}
+
+const render = (gl, numVertices) => {
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices)
+}
+
+const init = () => {
+  const canvas = document.getElementById('gl-canvas')
+
+  const gl = WebGLUtils.setupWebGL(canvas)
+  if (!gl) {
+    alert('WebGL isn\'t available')
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height)
+  gl.clearColor(1, 1, 1, 1)
+
+  // Load shaders and initialise attribute buffers
+  const program = initShaders(gl, 'vertex-shader', 'fragment-shader')
+  gl.useProgram(program)
+
+  // Create points
+  const numDivisions = 5,
+    tris = sierpinski(numDivisions),
+    numTris = tris.length
+
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(tris), gl.STATIC_DRAW)
+
+  const vPosition = gl.getAttribLocation(program, 'vPosition')
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(vPosition)
+
+  render(gl, numTris)
+}
+
+window.onload = init
+
